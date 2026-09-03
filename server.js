c8b83f019a077482348266b879f063e9ea1517b8
Krishna Majhi <96642265+krih1234560@users.noreply.github.com>
2026-09-03 09:41:57 +0545

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const pagesRouter = require('./routes/pages');
+const apiRouter = require('./routes/api');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.use('/api', apiRouter);
+app.use('/', pagesRouter);
+
+app.use((req, res) => {
+  res.status(404).send('Page not found');
+});
+
+app.listen(PORT, () => {
+  console.log(`Nepali Patro server running at http://localhost:${PORT}`);
+});
